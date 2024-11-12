daa1c88a4449ed924341f41c2c49fda6e17ff29d
wwwacedo <eng.marianammacedo@gmail.com>
2024-11-12 08:43:12 -0300

    finalizando procedural

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Screen Sound
 
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
-bandasRegistradas.Add("Linkin Park", new List<int> { 10, 9, 8, 10, 6 });
-bandasRegistradas.Add("Metallica", new List<int> { 10, 7, 8, 3, 6 });
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>> {
+	{ "Linkin Park", new List<int> { 10, 9, 8, 10, 6 } },
+	{ "Metallica", new List<int> { 10, 7, 8, 3, 6 } }
+};
+
 
 void ExibirLogo()
 // https://fsymbols.com/pt/geradores/
@@ -43,10 +45,10 @@ void ExibirOpcoesDoMenu()
 			MostrarBandasRegistradas();
 			break;
 		case 3:
-			Console.WriteLine("Avaliar uma banda");
+			AvaliarUmaBanda();
 			break;
 		case 4:
-			Console.WriteLine("Exibir a média de uma banda");
+			ExibirMediaDeUmaBanda();
 			break;
 		case -1:
 			Console.WriteLine("Saindo do programa");
@@ -78,12 +80,12 @@ void MostrarBandasRegistradas()
 	// {
 	// 	Console.WriteLine($"Banda: {listaDasBandas[i]}");;
 	// }
-	
+
 	foreach (var banda in bandasRegistradas.Keys)
 	{
 		Console.WriteLine($"Banda: {banda}");
 	}
-	
+
 	Console.Write("\nDigite qualquer tecla para voltar ao menu principal...");
 	Console.ReadKey();
 	Console.Clear();
@@ -92,11 +94,71 @@ void MostrarBandasRegistradas()
 
 void ExibirTituloDaOpcao(string titulo)
 {
-    int quantidadeDeLetras = titulo.Length;
-    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
-    Console.WriteLine(asteriscos);
-    Console.WriteLine(titulo);
-    Console.WriteLine(asteriscos + "\n");
+	int quantidadeDeLetras = titulo.Length;
+	string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+	Console.WriteLine(asteriscos);
+	Console.WriteLine(titulo);
+	Console.WriteLine(asteriscos + "\n");
+}
+
+void AvaliarUmaBanda()
+{
+	// digite qual banda deseja avaliar
+	// se a banda existir no diciionario, atribua a nota
+	// senao voltar ao menu principal
+	Console.Clear();
+	ExibirTituloDaOpcao("Avaliar uma banda");
+	Console.Write("Digite o nome da banda que deseja avaliar: ");
+	string nomeDaBanda = Console.ReadLine()!;
+
+	if (bandasRegistradas.ContainsKey(nomeDaBanda))
+	{
+		Console.Write("Digite a nota da banda: ");
+		int notaDaBanda = int.Parse(Console.ReadLine()!);
+		bandasRegistradas[nomeDaBanda].Add(notaDaBanda);
+		Console.WriteLine($"\nA banda {nomeDaBanda} foi avaliada com {notaDaBanda}!");
+		Thread.Sleep(4000);
+		Console.Clear();
+		ExibirOpcoesDoMenu();
+	}
+	else
+	{
+		Console.WriteLine($"\nA banda '{nomeDaBanda}' não foi encontrada!");
+		Console.Write("Pressione qualquer tecla para voltar ao menu principal...");
+		Console.ReadKey();
+		Console.Clear();
+		ExibirOpcoesDoMenu();
+	}
+
+}
+
+void ExibirMediaDeUmaBanda() {
+	Console.Clear();
+	ExibirTituloDaOpcao("Exibir média de uma banda");
+	// digitar o nome da banda
+	// verificar se a banda existe
+	// se existir, calcular a média
+	// se não existir, voltar ao menu principal
+	Console.Write("Digite o nome da banda que deseja verificar a média: ");
+	string nomeDaBanda = Console.ReadLine()!;
+
+	if (bandasRegistradas.ContainsKey(nomeDaBanda))
+	{
+		double media = bandasRegistradas[nomeDaBanda].Average();
+		Console.WriteLine($"\nA média da banda '{nomeDaBanda}' é {media}.");
+		Console.Write("\nPressione qualquer tecla para voltar ao menu principal...");
+		Console.ReadKey();
+		Console.Clear();
+		ExibirOpcoesDoMenu();
+	}
+	else
+	{
+		Console.WriteLine($"\nA banda '{nomeDaBanda}' não foi encontrada!");
+		Console.Write("\nPressione qualquer tecla para voltar ao menu principal...");
+		Console.ReadKey();
+		Console.Clear();
+		ExibirOpcoesDoMenu();
+	}
 }
 
 ExibirOpcoesDoMenu();
